6dc718b0ace984b9d8af18793ed1e038fceacd46
LIWEI-MINE <player2135@gmail.com>
2013-11-17 13:59:31 +0800

    auto jump

---
+++ Firefox/js/Greasemonkey/AutoJump.user.js
@@ -0,0 +1,11 @@
+﻿// ==UserScript==
+// @name        自动跳转到NGA.CC
+// @namespace   all
+// @include     http://nga.178.com/*
+// @version     1
+// ==/UserScript==
+
+
+var href=location.href;
+var newHref=href.replace("nga.178.com","bbs.ngacn.cc");
+location=newHref;
