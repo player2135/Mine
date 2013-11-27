55a065927b54e3b9e6119813ed60e348a8b622e8
LIWEI <player2135@gmail.com>
2013-11-27 11:23:50 +0800

    stopPropagation()

--- Firefox/js/Greasemonkey/19louBigPic.user.js
+++ Firefox/js/Greasemonkey/19louBigPic.user.js
@@ -5,12 +5,26 @@
 // @version     1
 // ==/UserScript==
 
-$(document).ready(function()
-{
-	$(".unlogin-img img").each(function()
+
+var unloginImgHolders=document.getElementsByClassName("unlogin-img");
+for	(var i=0;i<unloginImgHolders.length;i++)
+{	
+	unloginImgHolders[i].style.width="100%";
+	var imgs=unloginImgHolders[i].getElementsByTagName("img");
+	//$(unloginImgHolders[i]).unbind("click");
+	//$(unloginImgHolders[i].childNodes[0]).unbind("click");
+	for	(var j=0;j<imgs.length;j++)
 	{
-		var src=$(this).attr("src");
+		var src=imgs[j].getAttribute("src");
 		var newSrc=src.replace("thumb_","");
-		$(this).attr("src",newSrc);
-	});
-});
+		imgs[j].setAttribute("src",newSrc);
+		imgs[j].removeAttribute("width");
+		imgs[j].parentNode.removeChild(imgs[j].parentNode.childNodes[1]);
+		imgs[j].style.cursor="default";
+		imgs[j].onclick=function(e)
+		{
+			e.stopPropagation();
+		};
+		//$(imgs[j]).unbind("click");
+	}
+}
