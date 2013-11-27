62e1dbd12f03e384651b25772454e816af665c6b
LIWEI <player2135@gmail.com>
2013-11-27 09:44:54 +0800

    19lou big picture

---
+++ Firefox/js/Greasemonkey/19louBigPic.user.js
@@ -0,0 +1,16 @@
+﻿// ==UserScript==
+// @name        19楼直接看大图
+// @namespace   19lou
+// @include     http://www.19lou.com/*
+// @version     1
+// ==/UserScript==
+
+$(document).ready(function()
+{
+	$(".unlogin-img img").each(function()
+	{
+		var src=$(this).attr("src");
+		var newSrc=src.replace("thumb_","");
+		$(this).attr("src",newSrc);
+	});
+});
