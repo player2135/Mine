36ecb2d119ad63aa39d60c884d1b35a1e4c77c81
LIWEI-MINE <player2135@gmail.com>
2013-12-08 01:48:22 +0800

    add more pic to one page

--- Firefox/js/Greasemonkey/DisplayChapterName_qidian.user.js
+++ Firefox/js/Greasemonkey/DisplayChapterName_qidian.user.js
@@ -15,7 +15,7 @@ function DisplayChapterName()
     div.style.backgroundColor = "black";
 	div.style.color="#00ff00";
 	div.style.fontSize="16px";
-	div.innerHTML="<input type='button' value='图片化' onclick='window.ChangeTextToPicture();'/><br/>" + document.getElementById("lbChapterName").textContent;//"<input type='button' value='图片化' onclick='window.ChangeTextToPicture();'/><br/>" + 
+	div.innerHTML=document.getElementById("lbChapterName").textContent;//"<input type='button' value='图片化' onclick='window.ChangeTextToPicture();'/><br/>" + 
 	document.body.appendChild(div);
 }
 function SetNextPageHref()
@@ -32,15 +32,28 @@ window.ChangeTextToPicture = function()
 	var zj_txt=document.getElementsByClassName("zj_txt");
 	if(zj_txt.length!=0)
 	{
+		var textLength=zj_txt[0].textContent.length;
 		var zj_cont=zj_txt[0].parentNode;
 		zj_cont.removeChild(zj_txt[0]);
 		var div=document.createElement("div");
 		div.className="zj_img";
-		var img=document.createElement("img");
-		img.src=GetImgUrl(zj_cont.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
-		img.id="Image1";
 		zj_cont.insertBefore(div,zj_cont.childNodes[0]);
-		div.appendChild(img);
+		var imgs=GetImages(zj_cont.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode,textLength);
+		for(var i=0;i<imgs.length;i++)
+		{
+			div.appendChild(imgs[i]);
+		}
+	}
+	function GetImages(form1,textLength)
+	{
+		var imgArr=[];
+		for	(var i = 0;i<Math.ceil(textLength/2000);i++)
+		{
+			var img=document.createElement("img");
+			img.src=GetImgUrl(form1)+i.toString();
+			imgArr.push(img);
+		}
+		return imgArr;
 	}
 	function GetBookId()
 	{
@@ -68,7 +81,7 @@ window.ChangeTextToPicture = function()
 	}
 	function GetImgUrl(form1)
 	{
-		return "http://vip.qidian.com/BookReader/ChapterImage.aspx?bookId="+GetBookId()+"&chapterId="+GetChapterId(form1)+"&page="+GetPage(form1);
+		return "http://vip.qidian.com/BookReader/ChapterImage.aspx?bookId="+GetBookId()+"&chapterId="+GetChapterId(form1)+"&page=";
 	}
 }
 SetNextPageHref();
