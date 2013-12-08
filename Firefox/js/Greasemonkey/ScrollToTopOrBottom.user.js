be6a0d20e4b2dd7e68b4c07d487a2b0d7ca331b1
LIWEI-MINE <player2135@gmail.com>
2013-12-08 21:35:04 +0800

    change the opacity to 0.2

--- Firefox/js/Greasemonkey/ScrollToTopOrBottom.user.js
+++ Firefox/js/Greasemonkey/ScrollToTopOrBottom.user.js
@@ -18,7 +18,7 @@ function ScrollTopAndBottom()
 		img.style.left="915px";
 		img.style.bottom="0px";
 		img.style.fontSize="16px";
-		img.style.opacity="0.1";
+		img.style.opacity="0.2";
 		img.src="https://raw.github.com/player2135/Mine/Firefox/Firefox/img/toTop.png";
 		document.body.appendChild(img);
 	}
@@ -32,7 +32,7 @@ function ScrollTopAndBottom()
 		img.style.left="955px";
 		img.style.bottom="0px";
 		img.style.fontSize="16px";
-		img.style.opacity="0.1";
+		img.style.opacity="0.2";
 		img.src="https://raw.github.com/player2135/Mine/Firefox/Firefox/img/toBottom.png";
 		document.body.appendChild(img);
 	}
