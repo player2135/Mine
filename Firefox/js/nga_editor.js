a49309de7b7364c088e5897d0087782cdf7768f8
LIWEI <player2135@gmail.com>
2013-12-19 16:41:22 +0800

    try to fix bugs

--- Firefox/js/nga_editor.js
+++ Firefox/js/nga_editor.js
@@ -85,12 +85,10 @@ function nga_edit_Initialization(){
 	if (document.activeElement.id == "atc_content") txtisfocus = true;
 	var nga_edit_pathname = location.pathname;
 	if(nga_edit_pathname == '/post.php'){
-		var outerHTML=document.getElementById('atc_content').outerHTML;
-		var value=document.getElementById('atc_content').value;
 		var t_td = document.getElementById('atc_content').parentNode;
 		//try{document.getElementById("postform").appendChild(postfunc.o_content);}catch(e){}
 		t_td.innerHTML = nga_edit_gettabhtml();
-		try{document.getElementById("nga_edit_content").innerHTML+=outerHTML;document.getElementById('atc_content').value=value;}catch(e){console.log(e);}
+		try{document.getElementById("nga_edit_content").appendChild(postfunc.o_content);}catch(e){console.log(e);}
 		postfunc.o_content.style.width="99%";
 		document.getElementById("post_preview").style.display="inline";
 		document.getElementById("post_preview").style.padding="0";
