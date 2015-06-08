34520c6642a76e82964a74821ea24a81993411c4
Liwei <player2135@gmail.com>
2015-06-08 22:59:27 +0800

    qidian new vip reader title

--- Firefox/js/Greasemonkey/ChangeTitle_qidian.user.js
+++ Firefox/js/Greasemonkey/ChangeTitle_qidian.user.js
@@ -1,8 +1,14 @@
 ﻿// ==UserScript==
-// @name        修改VIP订阅的Title
+// @name        修改VIP的Title
 // @namespace   qidian
 // @include     http://vipreader.qidian.com/BookReader/BuyVIPChapterList.aspx*
+// @include		http://vipreader.qidian.com/BookReaderNew/vip,*.aspx
 // @version     1
 // ==/UserScript==
 
-document.title="*"+document.title.replace("订阅vip作品","").replace(" ","").replace("《","").replace("》","")+" 订阅vip作品";
+if (location.href.indexOf("http://vipreader.qidian.com/BookReader/BuyVIPChapterList.aspx") != -1) {
+	document.title = "*" + document.title.replace("订阅vip作品", "").replace(" ", "").replace("《", "").replace("》", "") + " 订阅vip作品";
+}
+if (location.href.indexOf("http://vipreader.qidian.com/BookReaderNew/vip,") != -1) {
+	document.title = document.title.replace("小说:", "");
+}
