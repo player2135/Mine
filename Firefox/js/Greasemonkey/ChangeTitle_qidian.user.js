566b18c2cf597cd6192837a275c08b7a89360f6a
Liwei <player2135@gmail.com>
2015-08-23 21:05:28 +0800

    BookReader

--- Firefox/js/Greasemonkey/ChangeTitle_qidian.user.js
+++ Firefox/js/Greasemonkey/ChangeTitle_qidian.user.js
@@ -3,13 +3,14 @@
 // @namespace   qidian
 // @include     http://vipreader.qidian.com/BookReader/BuyVIPChapterList.aspx*
 // @include		http://vipreader.qidian.com/BookReaderNew/vip,*.aspx
+// @include   http://vipreader.qidian.com/BookReader/vip,*.aspx
 // @version     1
 // ==/UserScript==
 
 if (location.href.indexOf("http://vipreader.qidian.com/BookReader/BuyVIPChapterList.aspx") != -1) {
 	document.title = "*" + document.title.replace("订阅vip作品", "").replace(" ", "").replace("《", "").replace("》", "") + " 订阅vip作品";
 }
-if (location.href.indexOf("http://vipreader.qidian.com/BookReaderNew/vip,") != -1) {
+if (location.href.indexOf("http://vipreader.qidian.com/BookReaderNew/vip,") != -1 || location.href.indexOf("http://vipreader.qidian.com/BookReader/vip,") != -1) {
 	document.title = document.title.replace("小说:", "");
 	var unselectCheckBoxes=function(){
 	var inputs=document.getElementsByTagName("input");
