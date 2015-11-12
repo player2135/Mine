cbd73ab395eacb46a669b969b7759f6a1787a147
Liwei <player2135@gmail.com>
2015-11-12 19:06:59 +0800

    RM-845 Nokia 822

--- Firefox/js/Greasemonkey/HighlightSomebody.user.js
+++ Firefox/js/Greasemonkey/HighlightSomebody.user.js
@@ -629,7 +629,11 @@ var highlightSomebody = function () {
 		}, {
 			MN : "RM-1049",
 			Version : "830"
-		}, ];
+		}, {
+			MN : "RM-845",
+			Version : "822"
+		}
+	];
 	var getLumiaVersion = function (manufacturerName) {
 		for (var i = 0; i < lumiaManufacturerNames.length; i++) {
 			if (manufacturerName.indexOf(lumiaManufacturerNames[i].MN) != -1) {
